9c114c999cf55a6512cc5571a97f8d3fe3b58889
Hilbert-Johnson <1730625285@qq.com>
2022-03-26 21:30:56 +0800

    rename

---
+++ NER.py
@@ -0,0 +1,205 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from functools import partial
+
+corpus = ["We always come to Paris",
+          "The professor is from Australia",
+          "I live in Stanford",
+          "He comes from Taiwan",
+          "The capital of Turkey is Ankara"]
+train_sentences = [sent.lower().split() for sent in corpus]
+
+locations = set(["australia", "ankara", "paris", "stanford", "taiwan", "turkey"])
+train_labels = [[1 if word in locations else 0 for word in sent] for sent in train_sentences]
+
+vocabulary = set(w for s in train_sentences for w in s)
+vocabulary.add("<unk>")
+vocabulary.add("<pad>")
+
+ix_to_word = sorted(list(vocabulary))
+word_to_ix = {word: ind for ind, word in enumerate(ix_to_word)}
+
+def pad_window(sentence, window_size, pad_token="<pad>"):
+    window = [pad_token] * window_size
+    return window + sentence + window 
+
+def convert_tokens_to_indices(sentence, word_to_ix):
+    return [word_to_ix.get(token, word_to_ix["<unk>"]) for token in sentence]
+
+def custom_collate_fn(batch, window_size, word_to_ix):
+  # Prepare the datapoints
+  x, y = zip(*batch)  
+  x = [pad_window(s, window_size=window_size) for s in x]
+  x = [convert_tokens_to_indices(s, word_to_ix) for s in x]
+
+  # Pad x so that all the examples in the batch have the same size
+  pad_token_ix = word_to_ix["<pad>"]
+  x = [torch.LongTensor(x_i) for x_i in x]
+  x_padded = nn.utils.rnn.pad_sequence(x, batch_first=True, padding_value=pad_token_ix)
+
+  # Pad y and record the length
+  lengths = [len(label) for label in y]
+  lenghts = torch.LongTensor(lengths)
+  y = [torch.LongTensor(y_i) for y_i in y]
+  y_padded = nn.utils.rnn.pad_sequence(y, batch_first=True, padding_value=0)
+
+  return x_padded, y_padded, lenghts 
+
+class WordWindowClassifier(nn.Module):
+    
+  def __init__(self, hyperparameters, vocab_size, pad_ix=0):
+    super(WordWindowClassifier, self).__init__()
+    
+    """ Instance variables """
+    self.window_size = hyperparameters["window_size"]
+    self.embed_dim = hyperparameters["embed_dim"]
+    self.hidden_dim = hyperparameters["hidden_dim"]
+    self.freeze_embeddings = hyperparameters["freeze_embeddings"]
+
+    """ Embedding Layer 
+    Takes in a tensor containing embedding indices, and returns the 
+    corresponding embeddings. The output is of dim 
+    (number_of_indices * embedding_dim).
+    If freeze_embeddings is True, set the embedding layer parameters to be
+    non-trainable. This is useful if we only want the parameters other than the
+    embeddings parameters to change. """
+    self.embeds = nn.Embedding(vocab_size, self.embed_dim, padding_idx=pad_ix)
+    if self.freeze_embeddings:
+      self.embeds.weight.requires_grad = False
+
+    """ Hidden Layer"""
+    full_window_size = 2 * window_size + 1
+    self.hidden_layer = nn.Sequential(
+      nn.Linear(full_window_size * self.embed_dim, self.hidden_dim), 
+      nn.Tanh()
+    )
+
+    """ Output Layer"""
+    self.output_layer = nn.Linear(self.hidden_dim, 1)
+
+    """ Probabilities """
+    self.probabilities = nn.Sigmoid()
+
+  def forward(self, inputs):
+    """
+    Let B:= batch_size
+        L:= window-padded sentence length
+        D:= self.embed_dim
+        S:= self.window_size
+        H:= self.hidden_dim
+    inputs: a (B, L) tensor of token indices"""
+    B, L = inputs.size()
+
+    """Reshaping.
+    Takes in a (B, L) LongTensor
+    Outputs a (B, L~, S) LongTensor"""
+    # Fist, get our word windows for each word in our input.
+    token_windows = inputs.unfold(1, 2 * self.window_size + 1, 1)
+    _, adjusted_length, _ = token_windows.size()
+
+    # Good idea to do internal tensor-size sanity checks, at the least in comments!
+    assert token_windows.size() == (B, adjusted_length, 2 * self.window_size + 1)
+
+    """Embedding.
+    Takes in a torch.LongTensor of size (B, L~, S) 
+    Outputs a (B, L~, S, D) FloatTensor."""
+    embedded_windows = self.embeds(token_windows)
+
+    """Reshaping.
+    Takes in a (B, L~, S, D) FloatTensor.
+    Resizes it into a (B, L~, S*D) FloatTensor.
+    -1 argument "infers" what the last dimension should be based on leftover axes."""
+    embedded_windows = embedded_windows.view(B, adjusted_length, -1)
+
+    """Layer 1.
+    Takes in a (B, L~, S*D) FloatTensor.
+    Resizes it into a (B, L~, H) FloatTensor"""
+    layer_1 = self.hidden_layer(embedded_windows)
+
+    """Layer 2
+    Takes in a (B, L~, H) FloatTensor.
+    Resizes it into a (B, L~, 1) FloatTensor."""
+    output = self.output_layer(layer_1)
+
+    """Softmax.
+    Takes in a (B, L~, 1) FloatTensor of unnormalized class scores.
+    Outputs a (B, L~, 1) FloatTensor of (log-)normalized class scores."""
+    output = self.probabilities(output)
+    output = output.view(B, -1)
+
+    return output
+
+# Prepare the data
+data = list(zip(train_sentences, train_labels))
+batch_size = 2
+shuffle = True
+window_size = 2
+collate_fn = partial(custom_collate_fn, window_size=window_size, word_to_ix=word_to_ix)
+
+# Instantiate a DataLoader
+loader = DataLoader(data, batch_size=batch_size, shuffle=shuffle, collate_fn=collate_fn)
+
+# Initialize a model
+# It is useful to put all the model hyperparameters in a dictionary
+model_hyperparameters = {
+    "batch_size": 4,
+    "window_size": 2,
+    "embed_dim": 25,
+    "hidden_dim": 25,
+    "freeze_embeddings": False,
+}
+
+vocab_size = len(word_to_ix)
+model = WordWindowClassifier(model_hyperparameters, vocab_size)
+
+# Define an optimizer
+learning_rate = 0.01
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+# Define a loss function, which computes to binary cross entropy loss
+def loss_function(batch_outputs, batch_labels, batch_lengths):   
+    # Calculate the loss for the whole batch
+    bceloss = nn.BCELoss()
+    loss = bceloss(batch_outputs, batch_labels.float())
+
+    # Rescale the loss. Remember that we have used lengths to store the 
+    # number of words in each training example
+    loss = loss / batch_lengths.sum().float()
+
+    return loss
+
+# Function that will be called in every epoch
+def train_epoch(loss_function, optimizer, model, loader):
+  
+  # Keep track of the total loss for the batch
+  total_loss = 0
+  for batch_inputs, batch_labels, batch_lengths in loader:
+    # Clear the gradients
+    optimizer.zero_grad()
+    # Run a forward pass
+    outputs = model.forward(batch_inputs)
+    # Compute the batch loss
+    loss = loss_function(outputs, batch_labels, batch_lengths)
+    # Calculate the gradients
+    loss.backward()
+    # Update the parameteres
+    optimizer.step()
+    total_loss += loss.item()
+
+  return total_loss
+
+
+# Function containing our main training loop
+def train(loss_function, optimizer, model, loader, num_epochs=10000):
+
+  # Iterate through each epoch and call our train_epoch function
+  for epoch in range(num_epochs):
+    epoch_loss = train_epoch(loss_function, optimizer, model, loader)
+    if epoch % 100 == 0: print(epoch_loss)
+    
+num_epochs = 1000
+train(loss_function, optimizer, model, loader, num_epochs=num_epochs)
+
+
+
