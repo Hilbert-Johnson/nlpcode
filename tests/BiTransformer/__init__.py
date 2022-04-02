ab416eb070ac8ea0f08353b43b511562a4890c4c
Hilbert-Johnson <1730625285@qq.com>
2022-04-02 11:36:36 +0800

    add model and task

--- tests/BiTransformer/__init__.py
+++ tests/BiTransformer/__init__.py
@@ -1,5 +1,9 @@
 from fairseq.models.transformer import TransformerModel, transformer_iwslt_de_en
 from fairseq.models import register_model, register_model_architecture
+from . import (
+    bidirectional_transformer as _,
+    bidirectional_translation_task as _,
+)
 
 @register_model('my_transformer')
 class MyTransformer(TransformerModel):
