47cef81c16ea8fe8c73fbd102727c577e39c0315
Hilbert-Johnson <1730625285@qq.com>
2022-04-02 15:15:08 +0800

    update .gitignore

--- tests/BiTransformer/__init__.py
+++ tests/BiTransformer/__init__.py
@@ -4,11 +4,3 @@ from . import (
     bidirectional_transformer as _,
     bidirectional_translation_task as _,
 )
-
-@register_model('my_transformer')
-class MyTransformer(TransformerModel):
-    pass
-
-@register_model_architecture('my_transformer', 'iwslt_arch')
-def my_transformer_iwslt(args):
-    transformer_iwslt_de_en(args)
